7ed8497e44ff1efcad57065035b4c772c019d12e
mxwilbur <mxwilbur@outlook.com>
2019-08-12 22:42:47 +0000

    added strikethrough and manage multiple clicks to same task

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -5,8 +5,10 @@ $(function() {
     // <li> tags
     function taskHtml(task) {
       var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
-        " data-id'" + task.id + "'" +
+      var liClass = task.done ? "completed" : "";
+      var liElement = '<li id="listItem-' + task.id +'"class="' + liClass + '">' +
+      '<div class="view"><input class="toggle" type="checkbox"' +
+      " data-id='" + task.id + "'" +
         checkedStatus +
         '><label>' +
         task.title +
@@ -31,6 +33,11 @@ $(function() {
         task: {
           done: doneValue
         }
+      }).success(function(data) {
+        var liHtml = taskHtml(data);
+        var $li = $("#listItem-" + data.id);
+        $li.replaceWith(liHtml);
+        $('.toggle').change(toggleTask);
       });
     }
 
